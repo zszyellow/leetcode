14145439d0d50a7dc16c690c89e754d320672d5b
zszyellow <2581113549@qq.com>
2017-10-29 18:07:19 -0400

    add problem 134, 136 & 137

---
+++ Cpp/134_gas_station/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start(0);
+        int total(0);
+        int tank(0);
+        for (int i = 0; i < gas.size(); i ++) 
+        {
+            if((tank = tank+gas[i]-cost[i]) < 0)
+            {
+                start = i + 1;
+                total += tank;
+                tank = 0;
+            }
+        }
+        return (total + tank < 0) ? -1 : start;
+    }
+};
