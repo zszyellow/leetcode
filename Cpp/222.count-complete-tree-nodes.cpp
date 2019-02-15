62db160d94356668dbdce93afa2a2285759fada5
zszyellow <2581113549@qq.com>
2019-02-14 20:24:57 -0500

    add two problems

---
+++ Cpp/222.count-complete-tree-nodes.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=222 lang=cpp
+ *
+ * [222] Count Complete Tree Nodes
+ *
+ * https://leetcode.com/problems/count-complete-tree-nodes/description/
+ *
+ * algorithms
+ * Medium (30.96%)
+ * Total Accepted:    104.9K
+ * Total Submissions: 338.5K
+ * Testcase Example:  '[1,2,3,4,5,6]'
+ *
+ * Given a complete binary tree, count the number of nodes.
+ * 
+ * Note: 
+ * 
+ * Definition of a complete binary tree from Wikipedia:
+ * In a complete binary tree every level, except possibly the last, is
+ * completely filled, and all nodes in the last level are as far left as
+ * possible. It can have between 1 and 2h nodes inclusive at the last level h.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * ⁠/ \  /
+ * 4  5 6
+ * 
+ * Output: 6
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root) return 1 + countNodes(root->left) + countNodes(root->right);
+        return 0;
+    }
+};
