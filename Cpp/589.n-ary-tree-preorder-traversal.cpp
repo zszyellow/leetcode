82ed28298ffd864a49c33f9fb216556c673443f3
zszyellow <2581113549@qq.com>
2019-02-23 21:35:59 -0500

    add three questions

---
+++ Cpp/589.n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=589 lang=cpp
+ *
+ * [589] N-ary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (65.94%)
+ * Total Accepted:    29.4K
+ * Total Submissions: 44.6K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Return its preorder traversal as: [1,3,5,6,2,4].
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        if (!root) return res;
+        res.push_back(root->val);
+        for (auto c : root->children) {
+            traverse(c, res);
+        }
+        return res;
+    }
+
+    void traverse(Node* cur, vector<int>& res) {
+        if (!cur) return;
+        res.push_back(cur->val);
+        for (auto c : cur->children) {
+            traverse(c, res);
+        }
+    }
+};
---
+++ Cpp/965.univalued-binary-tree.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=965 lang=cpp
+ *
+ * [965] Univalued Binary Tree
+ *
+ * https://leetcode.com/problems/univalued-binary-tree/description/
+ *
+ * algorithms
+ * Easy (67.79%)
+ * Total Accepted:    19.1K
+ * Total Submissions: 28.2K
+ * Testcase Example:  '[1,1,1,1,1,null,1]'
+ *
+ * A binary tree is univalued if every node in the tree has the same value.
+ * 
+ * Return true if and only if the given tree is univalued.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,1,1,1,1,null,1]
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [2,2,2,5,2]
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The number of nodes in the given tree will be in the range [1, 100].
+ * Each node's value will be an integer in the range [0, 99].
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if (!root) return true;
+        int origin = root->val;
+        return Traverse(root->left, origin) && Traverse(root->right, origin);
+    }
+    bool Traverse(TreeNode* cur, int origin) {
+        if (!cur) return true;
+        return origin == cur->val && Traverse(cur->left, origin) && Traverse(cur->right, origin);
+    }
+};
