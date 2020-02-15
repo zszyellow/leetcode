588df99f6783321fbcd87998cd8567486b913d5a
Shuze Zhou <zszyellow@hotmail.com>
2020-02-14 16:59:57 -0800

    add some problems

--- Cpp/51_N-Queens/solution.cpp
+++ Cpp/51_N-Queens/solution.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) 
-    {
-        vector<vector<string>> res;
-        vector<string> one_res;
-        vector<int> col;
-        traverse(n, 0, col, one_res, res);
-        return res;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result; // result vendor
+        vector<string> cur_result; // current result
+        vector<int> columns; // current collumns chosen
+        backtracking(n, 0, columns, cur_result, result);
+        return result;
     }
-    
-    // traverse the chess board to find the possible result
-    // Params:
-    // n       ->   the number of rows of the chess board
-    // cur_row ->   current row
-    // col     ->   the col vector 
-    // one_sol ->   one possible solution
-    // res     ->   all solutions
-    void traverse(int n, int cur_row, vector<int> &col, vector<string> &one_res, vector<vector<string>> &res) 
-    {
-        if (cur_row == n) 
-        {
-            res.push_back(one_res);
+
+    void backtracking(int n,
+                      int cur_row,
+                      vector<int> &columns,
+                      vector<string> &cur_result,
+                      vector<vector<string>> &result) {
+        if (cur_row == n) {
+            result.push_back(cur_result);
             return;
         }
-        
-        for (int cur_col = 0; cur_col < n; cur_col ++) 
-        {
-            if (valid(col, cur_row, cur_col)) 
-            {
-                string s(n, '.');
-                s[cur_col] = 'Q';
-                one_res.push_back(s);
-                col.push_back(cur_col);
-                traverse(n, cur_row + 1, col, one_res, res);
-                one_res.pop_back();
-                col.pop_back();
+        for (int cur_column = 0; cur_column < n; cur_column ++) {
+            string tmp_row(n, '.');
+            tmp_row[cur_column] = 'Q';
+            cur_result.push_back(tmp_row);
+            columns.push_back(cur_column);
+            if (isValid(columns, cur_row, cur_column)) {
+                backtracking(n, cur_row+1, columns, cur_result, result);
             }
+            cur_result.pop_back();
+            columns.pop_back();
         }
     }
-    
-    // check current position is valid or not
-    // Params:
-    // col     ->  the col vector
-    // cur_row ->  current row
-    // cur_col ->  current column
-    bool valid(vector<int> &col, int cur_row, int cur_col) 
-    {
-        // check if it is in the same row
-        if (cur_row < col.size()) return false;
-        // check if it is in the same col or in the same diagonal
-        for (int i = 0; i < col.size(); i ++) 
-        {
-            if (cur_col == col[i] || abs(cur_row-i) == abs(cur_col-col[i]))
+
+    bool isValid(vector<int> &columns,
+                 int cur_row,
+                 int cur_column) {
+        for (int i = 0; i < columns.size()-1; i ++) {
+            if (cur_column == columns[i] || 
+                abs(cur_row - i) == abs(cur_column - columns[i])) {
                 return false;
+            }
         }
         return true;
     }
+
 };
--- Cpp/52_N-Queens_II/solution.cpp
+++ Cpp/52_N-Queens_II/solution.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-        int res = 0;
-        vector<int> col;
-        traverse(n, 0, col, res);
-        return res;
+        int result = 0;
+        vector<int> columns;
+        backtracking(n, 0, columns, result);
+        return result;
     }
     
-    // traverse the chess board to find the possible result
-    // Params:
-    // n       ->   the number of rows of the chess board
-    // cur_row ->   current row
-    // col     ->   the col vector 
-    // res     ->   number of all solutions
-    void traverse(int n, int cur_row, vector<int> &col, int &res) 
-    {
-        if (cur_row == n) 
-        {
-            res ++;
+    void backtracking(int n, int cur_row, vector<int> &columns, int &result) {
+        if (cur_row == n) {
+            result ++;
             return;
         }
-        
-        for (int cur_col = 0; cur_col < n; cur_col ++) 
-        {
-            if (valid(col, cur_row, cur_col)) 
-            {
-                col.push_back(cur_col);
-                traverse(n, cur_row + 1, col, res);
-                col.pop_back();
+        for (int cur_col = 0; cur_col < n; cur_col ++) {
+            columns.push_back(cur_col);
+            if (isValid(columns, cur_row, cur_col)) {
+                backtracking(n, cur_row+1, columns, result);
             }
+            columns.pop_back();
         }
     }
     
-    // check current position is valid or not
-    // Params:
-    // col     ->  the col vector
-    // cur_row ->  current row
-    // cur_col ->  current column
-    bool valid(vector<int> &col, int cur_row, int cur_col) 
-    {
-        // check if it is in the same row
-        if (cur_row < col.size()) return false;
-        // check if it is in the same col or in the same diagonal
-        for (int i = 0; i < col.size(); i ++) 
-        {
-            if (cur_col == col[i] || abs(cur_row-i) == abs(cur_col-col[i]))
+    bool isValid(vector<int> &columns, int cur_row, int cur_col) {
+        for (int i = 0; i < columns.size()-1; i ++) {
+            if (columns[i] == cur_col ||
+                abs(cur_row - i) == abs(cur_col - columns[i])) {
                 return false;
+            }
         }
         return true;
     }
