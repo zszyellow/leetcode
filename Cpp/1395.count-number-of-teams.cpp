7965a809bd8662ea219fa64de1ffa86a0cbae9db
zszyellow <zszyellow@hotmail.com>
2020-10-14 03:32:11 -0700

    add comments

--- Cpp/1395.count-number-of-teams.cpp
+++ Cpp/1395.count-number-of-teams.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        /* 1. For every soldier, find how many soilders with larger and smaller rating in the left, get ll and ls
+           2. Then, find how many soiders with larger and smaller rating in the rightm get rl and rs
+           3. Yield result by accumulate ll*rs + ls*rl
+           TC: O(N^2), SC: O(1)
+        */
         int res = 0;
         for (int i = 1; i < rating.size()-1; i ++) {
             vector<int> left{0, 0};
